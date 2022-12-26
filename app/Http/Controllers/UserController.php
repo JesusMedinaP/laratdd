04d2fe48f3ce5b2d6c0e7443e910d9ecc1b19487
JesusMedinaP <jesusmedinaparraga@gmail.com>
2022-12-26 11:06:34 +0100

    Proyecto Laravel

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\{Http\Requests\CreateUserRequest, Http\Requests\UpdateUserRequest, Profession, Skill, User, UserProfile};
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::query()
+            ->with('team','skills','profile.profession')
+            ->when(request('team'), function ($query, $team) {
+                if ($team === 'with_team') {
+                    $query->has('team');
+                } elseif ($team === 'without_team') {
+                    $query->doesntHave('team');
+                }
+            })
+            ->filterBy(request()->all(['state', 'role', 'search']))
+            ->orderBy('created_at', 'DESC')
+            ->paginate();
+
+        $users->appends(request(['search','team', 'role', 'state']));
+
+        return view('users.index', [
+            'users' => $users,
+            'view' => 'index',
+            'skills' => Skill::orderBy('name')->get(),
+            'checkedSkills' => collect(request('skills')),
+        ]);
+    }
+
+    public function show(User $user)
+    {
+        if ($user == null) {
+            return response()->view('errors.404', [], 404);
+        }
+
+        return view('users.show', compact('user'));
+    }
+
+    protected function form($view, User $user)
+    {
+        return view($view, [
+            'user' => $user,
+            'professions' => Profession::orderBy('title', 'ASC')->get(),
+            'skills' => Skill::orderBy('name', 'ASC')->get(),
+        ]);
+    }
+
+    public function create()
+    {
+        return $this->form('users.create', new User);
+    }
+
+    public function store(CreateUserRequest $request)
+    {
+        $request->createUser();
+
+        return redirect()->route('users');
+    }
+
+    public function edit(User $user)
+    {
+        return $this->form('users.edit', $user);
+    }
+
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        $request->updateUser($user);
+
+        return redirect()->route('user.show', $user);
+    }
+
+    public function destroy($id)
+    {
+        $user = User::onlyTrashed()
+            ->where('id', $id)
+            ->firstOrFail();
+
+        $user->forceDelete();
+
+        return redirect()->route('users.trashed');
+    }
+
+    public function trash(User $user)
+    {
+        $user->profile()->delete();
+        $user->delete();
+
+        return redirect()->route('users');
+    }
+
+    public function trashed()
+    {
+        return view('users.index', [
+            'users' => User::onlyTrashed()->paginate(),
+            'view' => 'trash',
+        ]);
+    }
+
+}
