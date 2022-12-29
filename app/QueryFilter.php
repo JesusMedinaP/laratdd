2390938f2290277ac9b770b305f5994d22a5522f
JesusMedinaP <jesusmedinaparraga@gmail.com>
2022-12-29 12:47:37 +0100

    Paso 38

--- app/QueryFilter.php
+++ app/QueryFilter.php
@@ -7,6 +7,9 @@ use Illuminate\Support\Str;
 
 abstract class QueryFilter
 {
+
+    private $valid;
+
     abstract public function rules(): array;
 
     public function applyTo($query, array $filters)
@@ -17,7 +20,9 @@ abstract class QueryFilter
             array_intersect_key($filters, $rules), $rules
         );
 
-        foreach ($validator->valid() as $name => $value) {
+        $this->valid = $validator->valid();
+
+        foreach ($this->valid as $name => $value) {
             $this->applyFilters($query, $name, $value);
         }
 
@@ -33,5 +38,10 @@ abstract class QueryFilter
         } else {
             $query->where($name, $value);
         }
+    }
+
+    public function valid()
+    {
+        return $this->valid;
     }
 }
